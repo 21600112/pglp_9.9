98968d90a61816a8ad53aa9955baae9b535d6cba
Darksice <Darksice99@gmail.com>
2020-05-23 19:25:28 +0200

    ajout de tests unitaires

--- src/test/java/fr/uvsq/pglp_9_9/AppTest.java
+++ src/test/java/fr/uvsq/pglp_9_9/AppTest.java
@@ -1,38 +1,61 @@
 package fr.uvsq.pglp_9_9;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.Stack;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void carre() {
+		Point p = new Point(10,10);
+		Carre c = new Carre(p,"c1",10);
+		
+		Point pTransi = new Point(11,11);
+		c.deplace(pTransi);
+		
+		assertTrue(c.getSize() == 10);
+	}
+	
+	@Test
+	public void rectangle() {
+		Point p = new Point(10,10);
+		Rectangle r = new Rectangle(p,"r1",10,5);
+		
+		Point pTransi = new Point(11,11);
+		r.deplace(pTransi);
+		
+		int sizeL = r.getSizeL();
+		int sizeH = r.getSizeH();
+		
+		assertTrue(sizeL-5 == sizeH);
+	}
+	
+	@Test
+	public void Triangle() {
+		Point p = new Point(10,10);
+		Triangle t = new Triangle(p,"t1",10);
+		
+		Point pTransi = new Point(11,11);
+		t.deplace(pTransi);
+		
+		assertTrue(t.getSize() == 10);
+	}
+	
+	@Test
+	public void Cercle() {
+		Point p = new Point(10,10);
+		Cercle c = new Cercle(p,"c1",10);
+		
+		Point pTransi = new Point(11,11);
+		c.deplace(pTransi);
+		
+		assertTrue(c.getRayon() == 10);
+	}
+	
 }
